4d9f2531f94259daab6465f15599496336dafa4e
Felix Akpotu <56491908+LotusFelix@users.noreply.github.com>
2025-07-29 11:28:40 +0100

    Create __init__.py for algo_trader

---
+++ algo_trader/__init__.py
@@ -0,0 +1,14 @@
+# algo_trader/__init__.py
+"""
+Top‑level package for the algorithmic trading system.
+"""
+
+__version__ = "0.1.0"
+
+# You can optionally import high‑level classes here for easy access:
+# from .trading.scheduler import TradingBot
+
+__all__ = [
+    "config",
+    # "TradingBot",  # uncomment if importing TradingBot above
+]
